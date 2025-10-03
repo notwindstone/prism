6827e89fc0ee2af46d3c26e36a56bada3a832352
notwindstone <notwindstone@gmail.com>
2025-10-04 01:20:36 +0300

    fix: idk? github pages trigger not-found.vue

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -13,7 +13,7 @@ export const Routes = {
     "Key" : "Home",
     "Name": "routes.home.name",
     "Icon": Compass,
-    "Path": "/prism",
+    "Path": "/",
   },
   "Downloads": {
     "Key" : "Downloads",
